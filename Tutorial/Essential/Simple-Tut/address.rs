40be7c8b25c53c4655f8831c1d9d3a398d46f267
Axect <edeftg@gmail.com>
2018-04-12 17:42:25 +0900

    borrow pointer
    Former-commit-id: 797684e5537e9f7f77487f2546d615d0bdc8ceb8

--- Tutorial/Essential/Simple-Tut/address.rs
+++ Tutorial/Essential/Simple-Tut/address.rs
@@ -4,4 +4,13 @@ fn main() {
 
     println!("address of health-value: {:p}", &health);
     println!("address of game-value: {:p}", &game);
+
+    let game2 = &game;
+    println!("{:p}", game2);
+    println!("{}", *game2);
+
+    let mut score = 0;
+    let score2 = &mut score; // borrow score as mutable (only once!)
+    *score2 = 5;
+    println!("{}", *score2);
 }
