281b013189333d3340981b3ec8511d1511f5237a
Axect <edeftg@gmail.com>
2018-04-12 09:21:42 +0900

    Perfect gcd project
    Former-commit-id: 55f88d566ae60f770c0f469288305d76632cc45e

--- Tutorial/RustBook/gcd-1/src/main.rs
+++ Tutorial/RustBook/gcd-1/src/main.rs
@@ -1,5 +1,25 @@
 mod gcd;
 
+use gcd::gcd;
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    println!("gcd of 288, 324 is {}", gcd::gcd(288,324));
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0{
+        writeln!(std::io::stderr(), "Usage: gcd Number ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
