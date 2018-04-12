4c1ba0b718a0d206eea209f51b22f3e4ef2b2ceb
Axect <edeftg@gmail.com>
2018-04-12 18:20:38 +0900

    Successful Vim setup
    Former-commit-id: d5f29466558666d30902ce5db6b8adf590a0272f

--- Tutorial/Essential/Chap3/functions.rs
+++ Tutorial/Essential/Chap3/functions.rs
@@ -3,6 +3,9 @@ fn main() {
     let hero2 = "Riddick";
     greet(hero2);
     greet_both(hero1, hero2);
+
+    let power = increment_power(1);
+    println!("My power level is now: {}", power);
 }
 
 fn greet(name: &str) {
@@ -12,4 +15,9 @@ fn greet(name: &str) {
 fn greet_both(name1: &str, name2: &str) {
     greet(name1);
     greet(name2);
+}
+
+fn increment_power(power: i32) -> i32 {
+    println!("My Power is going to increse:");
+    power + 1
 }
