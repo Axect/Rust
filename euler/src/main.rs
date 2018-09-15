cc9257f2869245cf17ba672d44182a542da1fa71
Axect <edeftg@gmail.com>
2018-09-15 17:16:29 +0900

    Euler 2 with mod
    Former-commit-id: c33e538d6ac473819244b03b8f15823ae66fbef8

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -2,4 +2,5 @@ mod problems;
 
 fn main() {
     println!("{}", problems::p001_ans());
+    println!("{}", problems::p002_ans());
 }
--- euler/src/problems/mod.rs
+++ euler/src/problems/mod.rs
@@ -1,5 +1,10 @@
 pub mod p001;
+pub mod p002;
 
 pub fn p001_ans() -> u64 {
     p001::ans()
+}
+
+pub fn p002_ans() -> u64 {
+    p002::ans()
 }
