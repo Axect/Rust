51f755d4789c492157006b1d65d78810854d1c65
Axect <edeftg@gmail.com>
2018-09-17 13:48:59 +0900

    Break the loop
    Former-commit-id: bcd697a0c66d807bb1dcfc17af2abf5780ced940

--- Tutorial/Cute/rand_test/src/main.rs
+++ Tutorial/Cute/rand_test/src/main.rs
@@ -23,7 +23,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
