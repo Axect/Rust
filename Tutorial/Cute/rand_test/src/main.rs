f2a0328dfd6e96e7f04f917037b703c9183c7ebb
Axect <edeftg@gmail.com>
2018-09-17 13:48:19 +0900

    loop
    Former-commit-id: 4b8ab33fbcd3e9c50c13070cce9453d668dd1bd0

--- Tutorial/Cute/rand_test/src/main.rs
+++ Tutorial/Cute/rand_test/src/main.rs
@@ -8,20 +8,22 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    // println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You Guessed: {}", guess);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You Guessed: {}", guess);
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
