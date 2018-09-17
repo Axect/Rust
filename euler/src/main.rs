51df311c64adf9502e0fbbb7a9dfb6d464af02b9
Axect <edeftg@gmail.com>
2018-09-17 15:51:01 +0900

    Problem 3 Finish
    Former-commit-id: 8a082242021cda68115073fdb49300e191ee8126

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -3,4 +3,5 @@ mod problems;
 fn main() {
     println!("{}", problems::p001_ans());
     println!("{}", problems::p002_ans());
+    println!("{}", problems::p003_ans());
 }
--- euler/src/problems/mod.rs
+++ euler/src/problems/mod.rs
@@ -1,5 +1,6 @@
 pub mod p001;
 pub mod p002;
+pub mod p003;
 
 pub fn p001_ans() -> u64 {
     p001::ans()
@@ -7,4 +8,8 @@ pub fn p001_ans() -> u64 {
 
 pub fn p002_ans() -> u64 {
     p002::ans()
+}
+
+pub fn p003_ans() -> u64 {
+    p003::ans()
 }
