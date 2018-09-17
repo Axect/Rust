9c8ed3195721823dd0e97fc0c0a5ca90beb78ceb
Axect <edeftg@gmail.com>
2018-09-17 13:51:39 +0900

    Ok
    Former-commit-id: 9e439e0647a19559d2c2dc2930e4066a6bbbf629

--- Tutorial/Cute/rand_test/src/main.rs
+++ Tutorial/Cute/rand_test/src/main.rs
@@ -17,8 +17,10 @@ fn main() {
             .expect("Failed to read line");
         println!("You Guessed: {}", guess);
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
