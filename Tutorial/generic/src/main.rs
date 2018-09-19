8fcde33ada413ba75845ee8c6070a029c37e157d
Axect <edeftg@gmail.com>
2018-09-20 01:46:07 +0900

    Type cast as into
    Former-commit-id: ce3fe300b79f095d4e42c958b566d3e262bb38aa

---
+++ Tutorial/generic/src/main.rs
@@ -0,0 +1,26 @@
+use std::convert;
+fn main() {
+    let p = Matrix::new(1,2,vec![1,2,3,4]);
+    println!("{:?}", p);
+}
+
+#[derive(Debug)]
+pub struct Matrix {
+    i: u64,
+    j: u64,
+    data: Vec<f64>,
+}
+
+trait Setter<T: convert::Into<f64>> {
+    fn new(x:u64, y:u64, v: Vec<T>) -> Matrix;
+}
+
+impl<T> Setter<T> for Matrix where T: convert::Into<f64> {
+    fn new(x: u64, y: u64, v: Vec<T>) -> Matrix {
+        Matrix {
+            i: x,
+            j: y,
+            data: v.into_iter().map(|x| x.into()).collect(),
+        }
+    }
+}
