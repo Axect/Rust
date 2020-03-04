e6b436bf4ec7e3ca961ffcb6b84c448023e134ff
Axect <axect@outlook.kr>
2020-03-05 03:28:54 +0900

    Sparse CCS Structure

---
+++ sparse_ccs/src/lib.rs
@@ -0,0 +1,83 @@
+extern crate peroxide;
+use peroxide::*;
+use std::ops::Mul;
+
+/// CCS SParse Matrix
+#[derive(Debug, Clone)]
+pub struct SPMatrix {
+    pub row: usize,
+    pub col: usize,
+    pub nnz: usize,
+    pub col_ptr: Vec<usize>,
+    pub row_ics: Vec<usize>,
+    pub data: Vec<f64>,
+}
+
+impl SPMatrix {
+    pub fn new(row: usize, col: usize, nnz: usize) -> Self {
+        SPMatrix {
+            row,
+            col,
+            nnz,
+            col_ptr: vec![0usize; col + 1],
+            row_ics: vec![0usize, nnz],
+            data: vec![0f64; nnz],
+        }
+    }
+
+    pub fn from_dense(m: &Matrix) -> Self {
+        let mut data: Vec<f64> = Vec::new();
+        let mut row_ics: Vec<usize> = Vec::new();
+        let mut col_ptr: Vec<usize> = vec![0usize; m.col + 1];
+        let mut k = 0usize;
+        for j in 0 .. m.col {
+            for i in 0 .. m.row {
+                let val = m[(i,j)];
+                if val != 0f64 {
+                    data.push(val);
+                    row_ics.push(i);
+                    k += 1;
+                }
+            }
+            col_ptr[j+1] = k;
+        }
+
+        SPMatrix {
+            row: m.row,
+            col: m.col,
+            nnz: data.len(),
+            col_ptr,
+            row_ics,
+            data
+        }
+    }
+
+    pub fn col_ptr(&self) -> &Vec<usize> {
+        &self.col_ptr
+    }
+
+    pub fn row_ics(&self) -> &Vec<usize> {
+        &self.row_ics
+    }
+
+    pub fn data(&self) -> &Vec<f64> {
+        &self.data
+    }
+}
+
+// M * v = v
+impl Mul<Vec<f64>> for SPMatrix {
+    type Output = Vec<f64>;
+    fn mul(self, other: Vec<f64>) -> Self::Output {
+        let mut y = vec![0f64; self.row];
+        let col_ptr = self.col_ptr();
+        let row_ics = self.row_ics();
+        let data = self.data();
+        for j in 0 .. self.col {
+            for i in col_ptr[j] .. col_ptr[j+1] {
+                y[row_ics[i]] += data[i] * other[j];
+            }
+        }
+        y
+    }
+}
