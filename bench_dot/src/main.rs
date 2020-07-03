962dd393049e54e781a0511cfdaeb7b0cb668a67
Axect <axect@outlook.kr>
2020-07-03 15:56:44 +0900

    Fix

--- bench_dot/src/main.rs
+++ bench_dot/src/main.rs
@@ -3,22 +3,25 @@ use peroxide::fuga::*;
 use std::env::args;
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    let num: usize = args[1].parse().unwrap();
-    if num == 1 {
-        bench_1().print();
-    } else if num == 2 {
-        bench_2().print();
-    } else {
-        println!("err");
-    }
+    //let args: Vec<String> = args().collect();
+    //let num: usize = args[1].parse().unwrap();
+    //if num == 1 {
+    //    bench_1().print();
+    //} else if num == 2 {
+    //    bench_2().print();
+    //} else {
+    //    println!("err");
+    //}
+    // x^3
+    let x = vec![8f64, 12f64, 12f64, 6f64];
+    let y = vec![2f64, 1f64];
+
+    bench_1(&x, &y).print();
 }
 
-fn bench_1() -> Vec<f64> {
-    let x = seq(1, 2000, 1);
-    let y = seq(1, 1950, 1);
-    let mut z = vec![0f64; 2000];
-    for t in 0 .. 2000 {
+fn bench_1(x: &[f64], y:&[f64]) -> Vec<f64> {
+    let mut z = vec![0f64; x.len()];
+    for t in 0 .. x.len() {
         z[t] = if t < y.len() {
             x.iter().take(t+1).zip(y.iter().take(t+1).rev()).fold(0f64, |x, (x1, y1)| x + x1 * y1)
         } else {
