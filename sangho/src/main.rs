6bf0e0ff4266920e939f72be971bfba996f1a300
Axect <axect@outlook.kr>
2020-11-11 23:51:26 +0900

    Change directory

--- sangho/src/main.rs
+++ sangho/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     let a: Vec<usize> = vec![5,10,20,4,15,990,999,87];
-    println!("{}", count_zero(a.clone()));
+    println!("{}", count_zero(&a));
     println!("{}", a.into_iter().fold(1usize, |x, y| x * y));
 }
 
-fn count_zero(list: Vec<usize>) -> usize {
+fn count_zero(list: &[usize]) -> usize {
     let mut count_2 = 0usize;
     let mut count_5 = 0usize;
 
-    for elem in list.into_iter() {
+    for &elem in list.into_iter() {
         let mut temp = elem;
         while temp % 2 == 0 {
             count_2 += 1;
