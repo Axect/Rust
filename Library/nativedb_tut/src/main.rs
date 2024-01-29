5f76ce2e9d161fbec27741e03a65cb27678597a7
axect <ax2ct@outlook.com>
2024-01-29 15:30:39 +0900

    Read and Write is good but not update

--- Library/nativedb_tut/src/main.rs
+++ Library/nativedb_tut/src/main.rs
@@ -24,38 +24,75 @@ impl DBMatrix {
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let mut builder = DatabaseBuilder::new();
-    //builder.define::<DBMatrix>()?;
+fn write() -> Result<(), Box<dyn std::error::Error>> {
+    // Remove "matrix.db" if exists
+    std::fs::remove_file("matrix.db")?;
+    let mut builder = DatabaseBuilder::new();
+    builder.define::<DBMatrix>()?;
 
-    //let db = builder.create("matrix.db")?;
-    //let rw = db.rw_transaction().unwrap();
-    //let u = Uniform(0.0, 1.0);
-    //for i in 0 .. 10 {
-    //    for j in 0 .. 10 {
-    //        let m = u.sample(1)[0];
-    //        let matrix = rand(10, 10);
-    //        rw.insert(
-    //            DBMatrix::from_param_and_matrix((i, j), m, matrix)
-    //        )?;
-    //    }
-    //}
-    //
-    //let m = 0.1;
-    //let key = (1, 2);
-    //rw.insert(
-    //    DBMatrix::from_param_and_matrix(key, m, rand(10, 10))
-    //)?;
-    //rw.commit()?;
+    let db = builder.create("matrix.db")?;
+    let rw = db.rw_transaction().unwrap();
+    let u = Uniform(0.0, 1.0);
+    let mut m_vec = u.sample(100);
+    m_vec[1] = 0.1;
+    for i in 0 .. 10 {
+        for j in 0 .. 10 {
+            let m = m_vec[i * 10 + j];
+            let matrix = rand(10, 10);
+            rw.insert(
+                DBMatrix::from_param_and_matrix((i as u32, j as u32), m, matrix)
+            )?;
+        }
+    }
+    rw.commit()?;
 
+    Ok(())
+}
+
+fn update() -> Result<(), Box<dyn std::error::Error>> {
+    let mut builder = DatabaseBuilder::new();
+    builder.define::<DBMatrix>()?;
+
+    let db = builder.open("matrix.db")?;
+    let rw = db.rw_transaction().unwrap();
+    let rw2 = db.rw_transaction().unwrap();
+    let m = 0.1;
+    for item in rw.scan().secondary::<DBMatrix>(DBMatrixKey::m)?.start_with(m) {
+        println!("id: {:?}, m: {:.4}", item.id, item.m);
+        item.matrix.print();
+
+        let new_item = DBMatrix::from_param_and_matrix(item.id, item.m, zeros(10, 10));
+        println!("new id: {:?}, m: {:.4}", new_item.id, new_item.m);
+        new_item.matrix.print();
+        rw2.update(item, new_item)?;
+    }
+    rw2.commit()?;
+    rw.commit()?;
+
+    Ok(())
+}
+
+fn read() -> Result<(), Box<dyn std::error::Error>> {
     let mut builder = DatabaseBuilder::new();
     builder.define::<DBMatrix>()?;
     let db = builder.open("matrix.db")?;
     let r = db.r_transaction()?;
 
-    for item in r.scan().secondary::<DBMatrix>(DBMatrixKey::m)?.range(0.0..0.02) {
-        println!("id: {:?}, m: {:.4}", item.id, item.m);
-        item.matrix.print();
+    let item: DBMatrix = r.get().primary((0u32, 1u32))?.unwrap();
+    println!("id: {:?}, m: {:.4}", item.id, item.m);
+    item.matrix.print();
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let read_or_write = std::env::args().nth(1).unwrap();
+
+    match read_or_write.as_str().trim() {
+        "read" => read()?,
+        "update" => update()?,
+        "write" => write()?,
+        _ => panic!("Invalid argument"),
     }
 
     Ok(())
